1c87a1f432e933c048fe180b89e40024264a6b59
DereckMunozCbit <dereckmunoz@cbit-online.com>
2025-07-02 15:19:21 -0500

    Update App.java

--- src/main/java/org/example/aws/App.java
+++ src/main/java/org/example/aws/App.java
@@ -20,36 +20,5 @@ import java.util.Map;
 
 public class App {
 
-  public static void main(String[] args) throws Exception {
-    StaticCredentialsProvider creds = StaticCredentialsProvider.create(
-            AwsBasicCredentials.create("AKIAXQIQACOVAA6CJM5Q", "zSwbVSN5C6BQqoef4zJ+mq6bIo27fSa8WtR0LAtY")
-    );
-    AWSClientProvider provider = new AWSClientProvider(creds, Region.US_EAST_1);
-
-    DynamoDbService dynamo = new DynamoDbServiceImpl(provider);
-    SecretsManagerService secrets = new SecretsManagerServiceImpl(provider);
-    ParameterStoreService params  = new ParameterStoreServiceImpl(provider);
-    S3StorageService s3      = new S3StorageServiceImpl(provider);
-
-    String tabla = "prueba";
-    Map<String,AttributeValue> item = Map.of(
-            "id",     AttributeValue.builder().s("123").build(),
-            "nombre", AttributeValue.builder().s("Juan").build()
-    );
-    dynamo.putItem(tabla, item);
-
-    Map<String,AttributeValue> key = Map.of(
-            "id", AttributeValue.builder().s("123").build()
-    );
-    Map<String,AttributeValue> result = dynamo.getItem(tabla, key);
-    System.out.println("Recuperado: " + result);
-
-    String secretJson = secrets.getSecretValue("my-db-secret");
-    System.out.println("Secret: " + secretJson);
-
-    String param = params.getParameter("/myApp/dev/test", false);
-    System.out.println("Param: " + param);
-
-    s3.downloadObject("bucket-avc", "test.txt", Path.of("descargado.txt"));
-  }
+  public static void main(String[] args)  { }
 }
